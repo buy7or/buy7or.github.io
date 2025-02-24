355657e3a1049a656448006bc0f56694f27762e9
buy7or <142727783+buy7or@users.noreply.github.com>
2025-02-24 14:37:17 +0100

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://buy7or.github.io,
 	integrations: [mdx(), sitemap()],
 });
