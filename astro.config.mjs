ae377e70b3f5195394eef4f309020e97bd1a4159
diego <buy7or@gmail.com>
2025-03-02 18:04:29 +0100

    prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
-import sitemap from '@astrojs/sitemap';
+
 
 export default defineConfig({
   site: 'https://buy7or.github.io/',
-  integrations: [tailwind(), robotsTxt(), sitemap()],
+  integrations: [tailwind(), robotsTxt()],
 });
