71c69607358c442310983a250581c6c462835ef4
diego <buy7or@gmail.com>
2025-03-02 15:28:14 +0100

    url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://buy7or.github.io',
+  site: 'https://buy7or.github.io/',
   integrations: [tailwind(), robotsTxt(), sitemap()],
 });
