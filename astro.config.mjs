66eb0899cf217fbb5dab75276cb1480181d978dc
diego <buy7or@gmail.com>
2025-06-14 18:51:12 +0200

    doble barra quitada

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import robotsTxt from 'astro-robots-txt';
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://buy7or.github.io/',
+  site: 'https://buy7or.github.io',
   integrations: [
     tailwind(),
     sitemap(),
